c1ce81fb1cefc01ed5b529df75e40fa8b0733eee
Aswathiii <achuaswathi920@gmail.com>
2020-06-02 12:00:33 +0530

    Change3

--- tester.py
+++ tester.py
@@ -9,13 +9,13 @@ class Testing(unittest.TestCase):
     def setup(self):
         pass
 
-    def json_test(self):
+    def test_json(self):
         self.assertEqual(json_parser("data/db.json"), {'ACCT100': {'paid': 60, 'due': 100}, 'ACCT200': {'paid': 70, 'due': 60}, 'ACCT300': {'paid': 0, 'due': 0}} )
         
-    def xml_test(self):
+    def test_xml(self):
         self.assertEqual(xml_parser("data/db.xml"), {'ACCT400': {'paid': '600', 'due': '10000'}, 'ACCT500': {'paid': '70', 'due': '40'}, 'ACCT600': {'paid': '0', 'due': '0'}})
 
-    def yml_test(self):
+    def test_yml(self):
         self.assertEqual(yaml_parser("data/db.yml"), {'ACCT700': {'paid': 60, 'due': 100}, 'ACCT800': {'paid': 70, 'due': 60}, 'ACCT900': {'paid': 0, 'due': 0}})
 
 if __name__ =='__main__':
